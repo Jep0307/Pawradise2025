4c3e0db8ecdf4a87e47f81d2d364fa182a3d3449
Jeferson <muring.jeferson.ismael@gmail.com>
2025-04-05 00:54:48 +0800

    changed new ui

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,12 +10,37 @@ function toggleMenu() {
     navMenu.classList.remove("active"); // Hide the menu
     menuToggleIcon.src = "/Pawradise2025/assets/imgs/Menu-icon.svg"; // Show hamburger icon
     menuToggleIcon.alt = "Open Menu";
+    document.body.style.overflow = "auto"; // Allow scrolling
   } else {
     navMenu.classList.add("active"); // Show the menu
     menuToggleIcon.src = "/Pawradise2025/assets/imgs/X-icon.svg"; // Show close icon
     menuToggleIcon.alt = "Close Menu";
+    document.body.style.overflow = "hidden"; // Allow scrolling
   }
 }
 
 // Add event listener
 menuToggleIcon.addEventListener("click", toggleMenu);
+
+
+// Show and unshow the header on scroll
+const header = document.querySelector('header');
+let lastScrollY = window.scrollY;
+const isMenuOpen = navMenu.classList.contains("active");
+
+window.addEventListener('scroll', () => {
+
+  if(lastScrollY < window.scrollY){
+    header.style.opacity = '0';
+  }
+
+  else {
+    header.style.opacity = '1';
+  }
+
+  if(header && isMenuOpen){
+    isMenuOpen.classList.remove('active');
+  }
+
+  lastScrollY = window.scrollY;
+});
